8ad309b5fc1c2f34b5098c56252cae01b727821d
kelvinmuchui <muchuikelvin423@gmail.com>
2017-10-18 17:27:29 +0300

    added middleware

--- app.js
+++ app.js
@@ -4,12 +4,18 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var expressValidator = require("express-validator");
+var flash = require('connect-flash');
+var multer = require('multer');
+var session = require('express-session');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
 
+//app.use(multer({dest: './public/img/portfolio'}));
+var upload = multer({dest: './public/img/portfolio'});
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
@@ -22,6 +28,36 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Express session
+app.use(session({
+  secret : 'secret',
+  saveUnitialized: true,
+  save : true
+}));
+//connect -flash
+app.locals.moment = require('moment');
+app.use(function(req, res, next){
+res.locals.messages =require('express-messages')(req,res);
+  next();
+})
+
+app.use(expressValidator({
+  errorFormatter: function(param, msg, value){
+    var namespace = param.split('.')
+    ,root = namespace.shift()
+    ,formParam = root;
+
+    while(namespace.length){
+      formParam += '[' + namespace.shift() +']';
+
+    }
+    return{
+      param : formParam,
+      msg : msg,
+      value : value
+    };
+  }
+}))
 app.use('/', index);
 app.use('/users', users);
 
